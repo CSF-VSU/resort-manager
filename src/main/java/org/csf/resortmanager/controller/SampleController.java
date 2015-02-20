6d98535dac3d2c02e8db1b7bb011fed386f46786
Andrey Popikov <agpopikov@gmail.com>
2015-02-20 23:30:18 +0300

    minor changes in configuration

--- src/main/java/org/csf/resortmanager/controller/SampleController.java
+++ src/main/java/org/csf/resortmanager/controller/SampleController.java
@@ -3,7 +3,10 @@ package org.csf.resortmanager.controller;
 import org.csf.resortmanager.service.SampleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 /**
  * Created by agpopikov on 18/02/15.
@@ -15,7 +18,8 @@ public class SampleController {
     private SampleService sampleService;
 
     @RequestMapping("/sample")
-    public String sampleMethod() {
+    public String sampleMethod(ModelMap model) {
+        model.put("message", "Hello");
         return "sample/index";
     }
 
